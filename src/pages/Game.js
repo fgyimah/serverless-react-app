86236538da7f308354f7be240a4d3617b4b71978
Francis Gyimah <fgyimah883@gmail.com>
2020-06-14 12:49:09 +0000

    added the logic for the timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,18 +7,40 @@ import {
 } from '../styled/Game';
 import { Strong } from '../styled/Random';
 
-function Game() {
+function Game({ history }) {
+  const MAX_SECONDS = 5;
   const [score, setScore] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
+  const [ms, setMs] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setScore((prevScore) => prevScore + 1);
-    }, 1000);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
 
     return () => {
       clearInterval(interval);
     };
-  }, [score]);
+  }, []);
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push('/gameOver');
+    }
+  }, [seconds, history]);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const interval = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMsString = ('00000' + interval).slice(-5);
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMsString.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMsString.substring(formattedMsString.length - 3));
+
+    setSeconds(updatedSeconds.toString().padStart(2, 0));
+    setMs(updatedMs.toString().padStart(3, 0));
+  };
 
   return (
     <StyledGame>
@@ -27,7 +49,10 @@ function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time: <Strong>00: 000</Strong>
+        Time:{' '}
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
