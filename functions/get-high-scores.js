f58b71c482820651800b1157d608f3c6d4b87656
Francis Gyimah <fgyimah883@gmail.com>
2020-06-17 00:27:00 +0000

    added environment variables capabilities to our web app

--- functions/get-high-scores.js
+++ functions/get-high-scores.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const Airtable = require('airtable');
 
 Airtable.configure({
-  apiKey: 'keycc6T6KDZdiAV4U',
+  apiKey: process.env.AIRTABLE_API_KEY,
 });
 
-const base = Airtable.base('appMTPlesSk16mr6H');
+const base = Airtable.base(process.env.AIRTABLE_BASE_NAME);
 const table = base.table('Table 1');
 
 exports.handler = async (event, context) => {
